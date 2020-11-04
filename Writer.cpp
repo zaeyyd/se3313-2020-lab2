3c25d5c6be11b4ce77022eb050a529cdf04012f4
Zayyed <zayyedmansoor@gmail.com>
2020-11-03 21:24:10 -0500

    testing

--- Writer.cpp
+++ Writer.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <thread.h>
+#include <thread.cpp>
+#include <SharedObject.h>
 
-/*
+
+
 struct MyShared{
-	...;
-};*/
+	int threadId;
+	int reportId;
+	int timePassed;
+};
 
 int main(void)
 {
@@ -13,19 +19,19 @@ int main(void)
 	// This is a possible starting point for using threads and shared memory. 
 	// You do not have to start with this
 	////////////////////////////////////////////////////////////////////////
-	/*...
+	
 	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-	...
+	
 	while(true){
-		...
+		
 		//create thread using user input
-		thread1 = new WriterThread(xyz); //add arguments as needed
-		...
+		thread1 = new WriterThread(2); //add arguments as needed
+		
 	}
 	//example for one thread thread1
 	thread1->flag= true;
 	delete thread1;
-	*/
+	
 }
 
 
@@ -33,7 +39,7 @@ int main(void)
 // This is a possible starting point for using threads and shared memory. 
 // You do not have to start with this
 ////////////////////////////////////////////////////////////////////////
-/*class WriterThread : public Thread{
+class WriterThread : public Thread{
 	public:
 		int 	threadNum;
 		bool	flag;
@@ -43,17 +49,17 @@ int main(void)
 		}
 
 		virtual long ThreadMain(void) override{
-			...
+			
 			//declare shared memory var so this thread can access it
 			Shared<MyShared> sharedMemory ("sharedMemory");
 			while(true)
 			{
 				//write to shared memory
-				...  
+				
 				if(flag){//Exit loop to end the thread
 					break;
 				}
 			}
 		}
 };
-*/
+

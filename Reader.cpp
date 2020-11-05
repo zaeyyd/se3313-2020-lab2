d202c8c3ee76e90d76bd313343fdc362bf14ed56
Zayyed <zayyedmansoor@gmail.com>
2020-11-04 21:25:11 -0500

    got it working 2

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include "SharedObject.h"
+#include "Semaphore.h"
+
+using namespace std;
+
+struct MyShared{
+	int threadIdShared;
+	int reportIdShared;
+	int delayShared;
+	bool running;
+};
+
+
+
 
 int main(void)
 {
-	std::cout << "I am a reader" << std::endl;
+	cout << "I am a reader" << endl;
+
+	Shared<MyShared> sharedMemory ("sharedMemory");
+
+	Semaphore readSem("readerSemaphore");
+
+
+	while (true)
+	{
+
+
+		if(sharedMemory->running == true){
+			readSem.Wait();
+			cout << "Thread ID: " << sharedMemory->threadIdShared <<" Report ID: "<<sharedMemory->reportIdShared <<" Delay: " <<sharedMemory->delayShared << endl;
+			sleep(1);
+		}
+		else{
+			break;
+		}
+	}
+	
+	return 0;
 }
